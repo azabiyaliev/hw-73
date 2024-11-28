a982feeac756457d497b2c0551572cdc059c1971
azabiyaliev <aza.biyaliev@gmail.com>
2024-11-28 17:24:12 +0600

    Task two done.

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const Vigenere = require('caesar-salad').Vigenere;
+
+const password = "password";
+
 app.get('/', (req, res) => {
    return res.send('Welcome to the Tasks');
 });
 
 app.get('/:echo', (req, res) => {
     return res.send(`${req.params.echo}`);
+})
+
+app.get(`/encode/:encode` , (req, res) => {
+    return res.send(Vigenere.Cipher(password).crypt(req.params.code));
+})
+
+app.get(`/decode/:decode`, (req, res) => {
+    return res.send(Vigenere.Decipher(password).crypt(req.params.decode));
 })
 
 app.listen(port, () => {
